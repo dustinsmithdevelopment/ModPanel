44ba946006d10cb356a218d720013ea151ca721f
TechGryphon <TechGryphon@gmail.com>
2025-02-07 15:30:27 -0600

    added ModButton function to generate pressable buttons

--- ModTool.ts
+++ ModTool.ts
@@ -1,22 +1,67 @@
 import 'horizon/core';
-import {UIComponent, View, Text, UINode} from 'horizon/ui';
-let tempArray: UINode[] = [];
+import {UIComponent, View, Text, UINode, Binding, Callback, ViewStyle, Pressable} from 'horizon/ui';
 
-for (let i = 0; i < 32; i++) {
-  tempArray.push(Text({text: String(i+1), style: {fontSize:24, color: 'white', textAlign: 'center'}}));
+const header1 = Text({text: 'Mod Panel', style: {fontSize:24, color: 'yellow', textAlign: 'center'}})
+const header2 = Text({text: 'Created by TechGryphon', style: {fontSize:24, color: 'yellow', textAlign: 'center'}})
+const header3 = Text({text: ' ', style: {fontSize:24, color: 'yellow', textAlign: 'center'}})
+const footer = Text({text: ' ', style: {fontSize:24, color: 'yellow', textAlign: 'center'}})
+
+
+type MyButtonProps = {
+  label: Binding<String>,
+  onClick: Callback,
+};
+
+function ModButton(props: MyButtonProps): UINode {
+  return Pressable({
+    children: Text({
+      text: props.label,
+      style: { fontSize:24, color: 'white', textAlign: 'center' },
+    }),
+    onClick: props.onClick,
+    style: {
+      backgroundColor: 'black',
+      width: 500,
+      height: 30,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
 }
-const textNodes = [...tempArray];
-tempArray = [] as UINode[];
 
 
+// create an array of text bindings to store text and allow update.
+let tempTextBindings: Binding<String>[] = [];
+for (let i = 0; i < 32; i++) {
+  tempTextBindings.push(new Binding<String>(String(i+1)))
+}
+const textBindings = [...tempTextBindings];
+tempTextBindings = [] as Binding<String>[];
+function doNothing() {}
+
+// create an array of UINodes to display mod tool text
+let tempUINodeArray: UINode[] = [];
+for (let i = 0; i < 32; i++) {
+  tempUINodeArray.push(ModButton({label: textBindings[i], onClick: doNothing}));
+}
+const textNodes = [...tempUINodeArray];
+tempUINodeArray = [] as UINode[];
+
+
+
+
+
+
+const allPanelNodes: UINode[] = [header1, header2, header3, ...textNodes, footer];
 class ModTool extends UIComponent {
   panelName = 'ModTool';
-  panelHeight = 2000;
+  panelHeight = 1100;
   panelWidth = 500;
   
   initializeUI() {
     return View({
-      children: textNodes,
+
+      children: allPanelNodes,
       style: {backgroundColor: 'black'},
     });
   }
