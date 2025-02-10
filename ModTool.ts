c644260404819570a07b306d54a7e2ff91891e49
TechGryphon <TechGryphon@gmail.com>
2025-02-09 23:48:03 -0600

    Player can now be selected and primary options menu is displayed, back button is functional

--- ModTool.ts
+++ ModTool.ts
@@ -8,6 +8,25 @@ const worldOwner = 'TechGryphon';
 // Variables required
 // Group Name: ModPanel_Core
 // Variables as numbers: Role,
+const managerLevel = 50;
+const roles: { [key: string]: { name: string; color: string } }  = {
+  '100': {
+    name: 'Owner',
+    color: 'blue',
+  },
+  '50': {
+    name: 'Manager',
+    color: 'green',
+  },
+  '10': {
+    name: 'Mod',
+    color: 'green',
+  },
+  '0': {
+    name: 'Player',
+    color: 'white',
+  },
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -22,39 +41,46 @@ type MyButtonProps = {
   label: Binding<String>,
   onClick: Callback,
 };
-const roles: { [key: string]: { name: string; color: string } }  = {
-  '100': {
-    name: 'Admin',
-    color: 'blue',
-  },
-  '10': {
-    name: 'Mod',
-    color: 'green',
-  },
-  '0': {
-    name: 'Player',
-    color: 'white',
-  },
-}
 type ActivePlayer = {
   name: string;
   index: number;
 }
+
+interface MenuItem {
+  label: string;
+  onClick: () => void;
+  color: string;
+}
+
 function CoreKey(variableName: string) {
   return 'ModPanel_Core' + ':' + variableName;
 }
+
+
 class ModTool extends UIComponent {
+  private menuPages: MenuItem[] = [
+      {label: 'Back', onClick: ()=>{this.ShowPlayerList(); this.selectedPlayer = this.world.getServerPlayer()}, color: 'white'},
+      {label: 'Player Management', onClick: () => {return}, color: 'purple'},
+      {label: 'Teleport Options', onClick: () => {return}, color: 'green'},
+      {label: 'Voice Settings', onClick: () => {return}, color: 'yellow'},
+      {label: 'Kick Options', onClick: () => {return}, color: 'orange'},
+      {label: 'Player Movement', onClick: () => {return}, color: 'teal'},
+      {label: 'World Settings', onClick: () => {return}, color: 'blue'},
+      {label: 'Stats', onClick: () => {return}, color: 'limegreen'}
+  ]
+  // private pages: String[] = ['PlayerList', 'MenuOptions', 'WorldSettings', 'PlayerManagement', 'PlayerRoles', 'PlayerPermissions', 'PlayerRoomsAccess', 'VoiceSettings', 'TeleportOptions', 'KickOptions', 'PlayerMovement', 'Stats'];
   private currentPage = 'PlayerList'
-  private playerList: Player[] = [];
-  private selectedPlayer: Player | undefined;
+  private playerList: Player[] = new Array<Player>();
+  private selectedPlayer: Player | undefined = undefined;
   private header1 = Text({text: 'Mod Panel', style: {fontSize:24, color: 'yellow', textAlign: 'center'}})
   private header2 = Text({text: 'Created by TechGryphon', style: {fontSize:24, color: 'yellow', textAlign: 'center'}})
   private header3 = Text({text: ' ', style: {fontSize:24, color: 'yellow', textAlign: 'center'}})
-  private header4 = Text({text: ' ', style: {fontSize:24, color: 'yellow', textAlign: 'center'}})
+  private errorDisplay = Text({text: ' ', style: {fontSize:24, color: 'red', textAlign: 'center'}})
   SetError(error: String){
     // TODO set this up later to display errors
     return
   }
+
 
   ResolvePlayerColor(player:Player){
     const worldValues = this.world.persistentStorage
@@ -62,27 +88,21 @@ class ModTool extends UIComponent {
     return roles[String(playerModValue)].color;
   }
   private noPlayersText = [Text({text: 'No Players', style: {fontSize:24, color: 'yellow', textAlign: 'center'}})]
-
-  private pressableList:UINode[] = [];
-  displayPressableListBinding = new Binding<UINode[]>(this.pressableList);
-  CreatePressableList(){
-    this.pressableList = [];
+  displayPressableListBinding = new Binding<UINode[]>([]);
+  CreatePlayerList(){
+    let tempList: UINode[] = [];
     this.playerList.forEach((player: Player) => {
-      this.pressableList.push(Pressable({
+      tempList.push(Pressable({
         children: Text({text: player.name.get(), style: {color: this.ResolvePlayerColor(player), fontSize: 24, textAlign: 'center'}}),
         onClick: ()=>{
+          console.log(player.name.get() + " selected");
           this.selectedPlayer = player;
-          //TODO Call the options menu
+          this.ShowMenuOptions();
         }
         }));
     })
-    // this.displayPressableListBinding.set(this.pressableList);
+    this.displayPressableListBinding.set(tempList);
   }
-
-
-
-
-
 
 
 
@@ -90,7 +110,8 @@ class ModTool extends UIComponent {
   panelHeight = 1100;
   panelWidth = 500;
   start() {
-    this.playerList.push(...this.world.getPlayers())
+    this.playerList.push(...this.world.getPlayers());
+    this.selectedPlayer = this.world.getServerPlayer();
     this.connectCodeBlockEvent(
         this.entity,
         CodeBlockEvents.OnPlayerEnterWorld,
@@ -113,9 +134,11 @@ class ModTool extends UIComponent {
     if (!this.playerList.find((p:Player)=>p.name.get() == player.name.get())){
       this.playerList.push(player)
     }
+    // Update the displayed list of players if it is currently being accessed
     if (this.currentPage == 'PlayerList') {
       this.ShowPlayerList()
     }
+
 
   }
   PlayerExitWorld(player: Player) {
@@ -123,6 +146,7 @@ class ModTool extends UIComponent {
     if (this.playerList.find((p:Player)=>p.name.get() == player.name.get())){
       this.playerList.splice(this.playerList.indexOf(<Player>this.playerList.find((p: Player) => p.name.get() == player.name.get())),1)
     }
+    // Update the displayed list of players if it is currently being accessed
     if (this.currentPage == 'PlayerList') {
       this.ShowPlayerList()
     }
@@ -132,27 +156,28 @@ class ModTool extends UIComponent {
     if (this.playerList.length === 0) {
       this.displayPressableListBinding.set(this.noPlayersText);
     }else {
-      this.CreatePressableList();
-      this.displayPressableListBinding.set(this.pressableList);
+      this.CreatePlayerList();
     }
   }
-  // ShowPlayerMenu(){
-  //   // TODO make this show the actual menu
-  //   // validate that a player is selected
-  //   if (this.selectedPlayer.index !== -1) {
-  //     //validate that the user has not been replaced by another player
-  //     if (this.world.getPlayerFromIndex(this.selectedPlayer.index)?.name.get() != this.selectedPlayer.name) {
-  //       console.log("Player has been replaced")
-  //     }else {
-  //       console.log(this.selectedPlayer.name + " has been selected")
-  //     }
-  //   }
-  // }
+  ShowMenuOptions(){
+    this.currentPage = 'MenuOptions';
+    let tempList: UINode[] = [];
+    this.menuPages.forEach((page: MenuItem) => {
+      // TODO check the player level here
+      if (50 < managerLevel && page.label == 'Player Management') return;
+      tempList.push(Pressable({
+        children: Text({text: page.label, style: {color: page.color, fontSize: 24, textAlign: 'center'}}),
+        onClick: page.onClick
+      }));
+    })
+    this.displayPressableListBinding.set(tempList);
+  }
+
   initializeUI() {
 
     return View({
 
-      children:[this.header1, this.header2, this.header3, this.header4,DynamicList({data: this.displayPressableListBinding, renderItem : (pressableItem: UINode)=> {
+      children:[this.header1, this.header2, this.header3, this.errorDisplay,DynamicList({data: this.displayPressableListBinding, renderItem : (pressableItem: UINode)=> {
           return pressableItem;
         }, style: {width: 500,}})],
       style: {
