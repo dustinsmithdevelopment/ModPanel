e66294987e7909f48c685aef4f35649ee53e5431
TechGryphon <TechGryphon@gmail.com>
2025-02-19 21:45:19 -0600

    Adding Popup when ownership is taken

--- TakeModToolOwnership.ts
+++ TakeModToolOwnership.ts
@@ -1,4 +1,4 @@
-import {CodeBlockEvents, Component, Player} from 'horizon/core';
+import {CodeBlockEvents, Color, Component, Player} from 'horizon/core';
 import {CoreKey, WORLD_OWNER, ownerRoleValue} from './ModTool'
 
 class TakeModToolOwnership extends Component<typeof TakeModToolOwnership> {
@@ -8,6 +8,8 @@ class TakeModToolOwnership extends Component<typeof TakeModToolOwnership> {
     this.connectCodeBlockEvent(this.entity, CodeBlockEvents.OnPlayerEnterTrigger,(player:Player)=>{
       if (player.name.get() == WORLD_OWNER){
         this.world.persistentStorage.setPlayerVariable(player, CoreKey('Role'), ownerRoleValue);
+        this.world.ui.showPopupForPlayer(player,
+          'You are now the world owner. You can now manage the world.', 3, {backgroundColor: Color.black, fontColor: Color.white, fontSize: 2, showTimer: false})
       }
     })
   }
